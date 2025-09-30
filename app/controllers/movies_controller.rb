fb77e83e40abb28d3c034f0ce68cb162f7eb819e
Aimee Oh <ao2686@columbia.edu>
2025-09-30 14:46:49 +0000

    changes to index to old completed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,55 +7,96 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @movies = Movie.all
     @all_ratings = Movie.all_ratings
-  
-    if params[:ratings].present?
-      if params[:ratings].is_a?(Hash)
-        @ratings_to_show = params[:ratings].keys
-      else
-        @ratings_to_show = params[:ratings]
-      end
-      session[:ratings] = params[:ratings]
-    elsif params[:commit] == "Refresh"
-      @ratings_to_show = @all_ratings
-      session.delete(:ratings)
-    elsif session[:ratings].present?
-      if session[:ratings].is_a?(Hash)
-        @ratings_to_show = session[:ratings].keys
-      else
-        @ratings_to_show = session[:ratings]
-      end
-    else
-      @ratings_to_show = @all_ratings
+    @ratings_to_show = ratings_param
+    @sort = params[:sort_by] || session[:sort_by]
+
+    if ratings_changed? || sort_changed?
+      update_session
+      redirect_to movies_path(sort_by: @sort, ratings: @ratings_to_show) and return
     end
-  
-    # sort
-    if params[:sort_by].present?
-      @sort_by = params[:sort_by]
-      session[:sort_by] = @sort_by
-    elsif session[:sort_by].present?
-      @sort_by = session[:sort_by]
-    else
-      @sort_by = nil
-    end
-  
-    # redirect to RESTful URL if missing params
-    if (params[:ratings].nil? && params[:sort_by].nil?) &&
-       (session[:ratings].present? || session[:sort_by].present?)
-      redirect_to movies_path(sort_by: session[:sort_by], ratings: session[:ratings]) and return
-    end
-  
-    # movies + highlighting
+
     @movies = Movie.with_ratings(@ratings_to_show)
-    case @sort_by
-    when 'title'
-      @movies = @movies.order(:title)
+    apply_sorting
+  end
+
+  def ratings_param
+    params[:ratings] || session[:ratings] || Hash[@all_ratings.map { |r| [r, "1"] }]
+  end
+
+  def ratings_changed?
+    params[:ratings] != session[:ratings]
+  end
+
+  def sort_changed?
+    params[:sort_by] != session[:sort_by]
+  end
+
+  def update_session
+    session[:sort_by] = @sort
+    session[:ratings] = @ratings_to_show
+  end
+
+  def apply_sorting
+    case @sort
+    when "title"
       @title_header = 'hilite bg-warning'
-    when 'release_date'
-      @movies = @movies.order(:release_date)
+      @movies = @movies.order(:title)
+    when "release_date"
       @release_date_header = 'hilite bg-warning'
+      @movies = @movies.order(:release_date)
     end
-  end 
+  end
+
+  # def index
+  #   @all_ratings = Movie.all_ratings
+
+  #   # --- ratings ---
+  #   if params[:ratings].present?
+  #     if params[:ratings].is_a?(Hash)
+  #       @ratings_to_show = params[:ratings].keys
+  #       session[:ratings] = params[:ratings]
+  #     else # Array
+  #       @ratings_to_show = params[:ratings]
+  #       session[:ratings] = Hash[@ratings_to_show.map { |r| [r, "1"] }]
+  #     end
+  #   elsif params[:commit] == "Refresh"
+  #     @ratings_to_show = @all_ratings
+  #     session.delete(:ratings)
+  #   elsif session[:ratings].present?
+  #     @ratings_to_show = session[:ratings].keys
+  #   else
+  #     @ratings_to_show = @all_ratings
+  #   end
+
+  #   # --- sort ---
+  #   if params[:sort_by].present?
+  #     @sort_by = params[:sort_by]
+  #     session[:sort_by] = @sort_by
+  #   elsif session[:sort_by].present?
+  #     @sort_by = session[:sort_by]
+  #   else
+  #     @sort_by = nil
+  #   end
+
+  #   # --- redirect to RESTful URL if missing params ---
+  #   if (params[:ratings].nil? && params[:sort_by].nil?) &&
+  #     (session[:ratings].present? || session[:sort_by].present?)
+  #     redirect_to movies_path(sort_by: session[:sort_by], ratings: session[:ratings]) and return
+  #   end
+
+  #   # --- movies + highlighting ---
+  #   @movies = Movie.with_ratings(@ratings_to_show)
+  #   case @sort_by
+  #   when 'title'
+  #     @movies = @movies.order(:title)
+  #     @title_header = 'hilite bg-warning'
+  #   when 'release_date'
+  #     @movies = @movies.order(:release_date)
+  #     @release_date_header = 'hilite bg-warning'
+  #   end
+  # end 
 
   def new
     # default: render 'new' template
